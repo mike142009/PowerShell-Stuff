b72c08de8a716dc27d552981a29ec5547a87f91f
mike142009 <90269779+mike142009@users.noreply.github.com>
2021-09-07 12:33:41 -0700

    Add files via upload

---
+++ PasswordSetWithin30Days.ps1
@@ -0,0 +1,9 @@
+﻿<# Finds the password of all AD users that have set there password
+ in the last 30 days
+#>
+
+
+Get-ADUser -Filter * -Properties passwordlastset | select name,passwordlastset
+$today= Get-Date
+$30DaysAgo= $today.AddDays(-30)
+Get-ADUser -filter "passwordlastset -lt '$30daysago'"
