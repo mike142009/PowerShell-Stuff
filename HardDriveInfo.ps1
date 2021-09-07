cc5a99aa8744c2d2a91dfb4f026c1f2d5bdfcabe
mike142009 <90269779+mike142009@users.noreply.github.com>
2021-09-07 09:57:10 -0700

    Add files via upload
    Gets hard drive info on remote computers

---
+++ HardDriveInfo.ps1
@@ -0,0 +1,18 @@
+﻿param (
+    [string]$CompName=(Read-Host "Enter the computer name:"),
+    [string]$AlternateCreds
+
+)
+
+If($AlternateCreds -eq $true) 
+{
+    $cred= Get-Credential
+
+    $session= New-CimSession -ComputerName $CompName -Credential $cred
+    Get-CimInstance Win32LogicalDisk -CimSession $session -filter "DriveType=3"
+}
+Else
+{
+Get-CimInstance Win32_LogicalDisk -ComputerName $CompName -Filter "DriveType=3"
+}
+
