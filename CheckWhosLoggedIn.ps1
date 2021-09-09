e7778ff47f00642861b6db3a8a89c6d026e07c14
mike142009 <90269779+mike142009@users.noreply.github.com>
2021-09-09 14:04:47 -0700

    Add files via upload

---
+++ CheckWhosLoggedIn.ps1
@@ -0,0 +1,20 @@
+﻿#Working on a script to see who is logged into a computer
+
+
+function Get-LoggedIn  {
+    
+    [CmdletBinding()]
+    param (
+            
+            [Parameter (Mandatory=$true)]
+            [string[]] $ComputerName
+        )
+
+        foreach ($pc in $ComputerName) {
+                $logged_in = (gwmi win32_computersystem -ComputerName $pc).username
+                $name = $logged_in.split("\")[1]
+                "{0}: {1}" -f $pc,$name
+
+                }
+
+        }
