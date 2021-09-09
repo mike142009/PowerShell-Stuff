791e96b2e4a13ddd1af9b701b56837b15afb98d2
mike142009 <90269779+mike142009@users.noreply.github.com>
2021-09-09 13:36:39 -0700

    Add files via upload

---
+++ StartService.ps1
@@ -0,0 +1,16 @@
+﻿#-----------------------------------------------------
+# Starts a service on a server
+#
+#-----------------------------------------------------
+
+$Servers = Get-Content C:\Users\Administrator.ADATUM\Desktop\SER.txt
+
+Invoke-Command -ComputerName $Servers -ScriptBlock  {
+
+                If ((Get-Service -Name BITS).Status -ne "Running") {
+                #change name to start different service
+                Start-Service -Name DmEnrollmentSvc
+
+                }
+
+        }
